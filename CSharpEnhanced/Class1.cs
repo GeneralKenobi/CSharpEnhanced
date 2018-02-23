70cd1ae3a98e5caa205d04b5c868d4205fe9d1cb
Szymon Żogała <szymek.655@gmail.com>
2018-02-23 21:30:21 +0100

    Added INotifyPropertyChangedImplemented

--- CSharpEnhanced/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CSharpEnhanced
-{
-    public class Class1
-    {
-    }
-}
---
+++ CSharpEnhanced/CoreClasses/INotifyPropertyChangedImplemented/INotifyPropertyChangedImplemented.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CSharpEnhanced.CoreClasses
+{
+	/// <summary>
+	/// Base class for classes that don't derive from anything and want to implement <see cref="INotifyPropertyChanged"/>
+	/// </summary>
+	class INotifyPropertyChangedImplemented : INotifyPropertyChanged
+	{
+		#region Property Changed Event
+
+		/// <summary>
+		/// Property Changed Event
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Invokes Property Changed Event for each string parameter
+		/// </summary>
+		/// <param name="propertyName">Properties to invoke for. Null or string.Empty will result in notification for all properties</param>
+		public void InvokePropertyChanged(params string[] propertyNames)
+		{
+			foreach (var item in propertyNames)
+			{
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(item));
+			}
+		}
+
+		#endregion
+	}
+}
