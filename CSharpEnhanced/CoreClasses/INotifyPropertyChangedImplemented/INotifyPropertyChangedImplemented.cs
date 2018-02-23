fe0acc6a9c6c6b5ad96238628881877f20c27694
Szymon Żogała <szymek.655@gmail.com>
2018-02-23 21:35:10 +0100

    Added Delayed Invoker for Property Changed

--- CSharpEnhanced/CoreClasses/INotifyPropertyChangedImplemented/INotifyPropertyChangedImplemented.cs
+++ CSharpEnhanced/CoreClasses/INotifyPropertyChangedImplemented/INotifyPropertyChangedImplemented.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CSharpEnhanced.CoreClasses
 {
@@ -19,7 +20,7 @@ namespace CSharpEnhanced.CoreClasses
 
 		#endregion
 
-		#region Public methods
+		#region Property Changed Invoke
 
 		/// <summary>
 		/// Invokes Property Changed Event for each string parameter
@@ -32,6 +33,51 @@ namespace CSharpEnhanced.CoreClasses
 				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(item));
 			}
 		}
+
+		#endregion
+
+		#region Delayed On Property Changed
+
+		/// <summary>
+		/// Fires property changed event for the given property after the delay
+		/// </summary>
+		/// <param name="propertyNames">Names of the property to notify for</param>
+		/// <param name="delay">Delay before the property changed is fired, in ms (0-10000)</param>
+		/// <returns></returns>
+		public async Task InvokePropertyChangedDelayed(int delay, params string[] propertyNames)
+		{
+			// Check for the minimum value
+			if (delay < MinimumDelay)
+			{
+				delay = MinimumDelay;
+			}
+
+			// Check for the maximum value
+			if (delay > MaximumDelay)
+			{
+				delay = MaximumDelay;
+			}
+
+			// Await the delay
+			await Task.Delay(delay);
+
+			// Fire the property changed event
+			InvokePropertyChanged(propertyNames);
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Minimum delay for <see cref="InvokePropertyChangedDelayed(int, string[])"
+		/// </summary>
+		public static int MinimumDelay => 0;
+
+		/// <summary>
+		/// Maximum delay for <see cref="InvokePropertyChangedDelayed(int, string[])"/>
+		/// </summary>
+		public static int MaximumDelay => 10000;
 
 		#endregion
 	}
