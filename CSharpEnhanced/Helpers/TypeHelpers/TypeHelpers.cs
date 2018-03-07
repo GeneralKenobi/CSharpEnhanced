ec3bfd6dda8737ca42b07aabd9e222f229a826af
Szymon Żogała <szymek.655@gmail.com>
2018-03-07 23:04:04 +0100

    Added TypeHelpers helper class with method checking if a type derives from / is other type

---
+++ CSharpEnhanced/Helpers/TypeHelpers/TypeHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpEnhanced.Helpers
+{
+	/// <summary>
+	/// Class containing methods related to types
+	/// </summary>
+    public static class TypeHelpers
+    {
+		/// <summary>
+		/// Returns true if <paramref name="toCheck"/> either derives from <paramref name="baseType"/> or is <paramref name="baseType"/>
+		/// </summary>
+		/// <param name="toCheck"></param>
+		/// <param name="baseType"></param>
+		/// <returns></returns>
+		public static bool IsDerivedOrSelf(Type toCheck, Type baseType) => toCheck.IsSubclassOf(baseType) || toCheck == baseType;
+	}
+}
