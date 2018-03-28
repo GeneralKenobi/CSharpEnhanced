bd19b2f7a99f3273eff2242847452a286024e5a9
Szymon Żogała <szymek.655@gmail.com>
2018-03-28 17:55:20 +0200

    Added method parsing string into an arbirtrary number type

--- CSharpEnhanced/Helpers/TypeHelpers/TypeHelpers.cs
+++ CSharpEnhanced/Helpers/TypeHelpers/TypeHelpers.cs
@@ -16,5 +16,41 @@ namespace CSharpEnhanced.Helpers
 		/// <param name="baseType"></param>
 		/// <returns></returns>
 		public static bool IsDerivedOrSelf(this Type toCheck, Type baseType) => toCheck.IsSubclassOf(baseType) || toCheck == baseType;
+
+		/// <summary>
+		/// Tries to convert the string literal to a number type (in the following order: int, double, long). Returns true on success
+		/// </summary>
+		/// <param name="str">String that may be a number</param>
+		/// <returns></returns>
+		public static bool TryParseNumber(string str, out object result)
+		{
+			// Try to convert to in
+			if(int.TryParse(str, out int i))
+			{
+				result = i;
+			}
+			
+			// Otherwise try to convert to double
+			else if(double.TryParse(str, out double d))
+			{
+				result = d;
+			}
+
+			// Otherwise try to convert to long
+			else if(long.TryParse(str, out long l))
+			{
+				result = l;
+			}
+
+			// If nothing succeeded assign null and return false
+			else
+			{
+				result = null;
+				return false;
+			}
+
+			// If there was a success then return true
+			return true;
+		}
 	}
 }
