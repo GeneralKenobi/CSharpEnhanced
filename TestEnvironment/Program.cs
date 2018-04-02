9871c8e622acff5642a6aa46d3d06393f9f8d1a0
Szymon Żogała <szymek.655@gmail.com>
2018-04-02 13:24:06 +0200

    Testing the semaphores

--- TestEnvironment/Program.cs
+++ TestEnvironment/Program.cs
@@ -12,24 +12,62 @@ namespace TestEnvironment
 		static SemaphoreSlimFIFOTimeout l = new SemaphoreSlimFIFOTimeout(0,5);
 		static void Main(string[] args)
 		{
-			var s = new SemaphoreSlim(0);
-			CancellationTokenSource source = new CancellationTokenSource();
-			var t = source.Token;
-		
-			Task.Run(async() => { await Task.Delay(1000); s.Release(); }) ;
-
-
-
-			for (int i=1; i<6; ++i)
+			//Task.Run(() =>
+			//{
+			//	Thread.Sleep(1000);
+			//	Console.WriteLine("Standard Task waiting...");
+			//	l.Wait();
+			//	Console.WriteLine("Standard Task went through...");
+			//});
+			Task.Run(() =>
 			{
-				Test(i);
-			}
+				Thread.Sleep(1500);
+				Console.WriteLine("Timeout Task waiting...");
+				l.Wait();
 
-			while(true)
+			});
+			Task.Run(() =>
 			{
-				Console.ReadLine();
-				l.Release();
-			}
+				Console.WriteLine("Timeout Task waiting...");
+				if(l.Wait(1000))
+					Console.WriteLine("Timeout Task went through...");
+				else
+					Console.WriteLine("Timeout Task timed out...");
+
+			});
+			Task.Run(() =>
+			{
+				Console.WriteLine("Timeout Task waiting...");
+				if (l.Wait(1000))
+					Console.WriteLine("Timeout Task went through...");
+				else
+					Console.WriteLine("Timeout Task timed out...");
+
+			});
+			
+
+			Thread.Sleep(2000);
+			int gen = l.Waiting;
+
+			int act = l.ActiveWaiters();
+
+			int inact = l.TimedoutWaiters();
+
+			//Task.Run(() =>
+			//{
+			//	Thread.Sleep(1500);
+			//	l.Release();
+			//});
+
+			Console.ReadLine();
+
+			l.Release();
+			Thread.Sleep(1000);
+			gen = l.Waiting;
+
+			act = l.ActiveWaiters();
+
+			inact = l.TimedoutWaiters();
 		}
 
 		private static async void Test(int counter)
