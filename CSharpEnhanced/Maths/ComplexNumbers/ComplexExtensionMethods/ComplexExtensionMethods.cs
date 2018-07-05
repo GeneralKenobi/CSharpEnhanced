8bb9ae0f2a8bb07ca35939bf32894415bc92fbd0
Szymon Żogała <szymek.655@gmail.com>
2018-07-05 12:23:33 +0200

    Added an extension method class for System.Numerics.Complex and added a round method to it

---
+++ CSharpEnhanced/Maths/ComplexNumbers/ComplexExtensionMethods/ComplexExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace CSharpEnhanced.Maths
+{
+	/// <summary>
+	/// Extension methods for <see cref="Complex"/>
+	/// </summary>
+    public static class ComplexExtensionMethods
+    {
+		/// <summary>
+		/// Returns a complex number whose components are rounded to the nearest multiple of <paramref name="roundTo"/>
+		/// </summary>
+		/// <param name="complex">Complex number to round</param>
+		/// <param name="roundTo">Number whose multiple to round to</param>
+		/// <returns></returns>
+		public static Complex RoundTo(this Complex complex, double roundTo) =>
+			new Complex(Math.Round(complex.Real / roundTo) * roundTo, Math.Round(complex.Imaginary / roundTo) * roundTo);
+	}
+}
