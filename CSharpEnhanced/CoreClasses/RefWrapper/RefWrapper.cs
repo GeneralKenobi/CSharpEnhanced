38a185d755fc5b9d71b6d7f4744aba3e4f3713df
Szymon Żogała <szymek.655@gmail.com>
2018-07-10 22:10:36 +0200

    Added

---
+++ CSharpEnhanced/CoreClasses/RefWrapper/RefWrapper.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpEnhanced.CoreClasses
+{
+	/// <summary>
+	/// Class that may be used to wrap non-reference classes to be able to store them by reference
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class RefWrapper<T>
+    {
+		/// <summary>
+		/// Stored value
+		/// </summary>
+		public T Value { get; set; }
+    }
+}
