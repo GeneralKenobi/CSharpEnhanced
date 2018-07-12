c0a0ed548a8acf2da8c2d76713ca274dd8b4e63c
Szymon Żogała <szymek.655@gmail.com>
2018-07-12 09:31:17 +0200

    Added constructor and some static constants

--- CSharpEnhanced/Maths/Arithmetics/Variable/Variable.cs
+++ CSharpEnhanced/Maths/Arithmetics/Variable/Variable.cs
@@ -8,8 +8,64 @@ namespace CSharpEnhanced.Maths
 	/// </summary>
 	public class Variable : RefWrapper<Complex>
     {
+		#region Constructor
+
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public Variable() { }
+
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public Variable(Complex value)
+		{
+			Value = value;
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// True if the imaginary component is equal to 0
+		/// </summary>
 		public bool IsPureReal => Value.Imaginary == 0;
 
+		/// <summary>
+		/// True if the real component is equal to 0
+		/// </summary>
 		public bool IsPureImaginary => Value.Real == 0;
-    }
+
+		#endregion
+
+		#region Public static properties
+
+		/// <summary>
+		/// Pure real one
+		/// </summary>
+		public static Variable One { get; } = new Variable(Complex.One);
+		
+		/// <summary>
+		/// Pure real negative one
+		/// </summary>
+		public static Variable NegativeOne { get; } = new Variable(-Complex.One);
+
+		/// <summary>
+		/// Zero
+		/// </summary>
+		public static Variable Zero { get; } = new Variable(Complex.Zero);
+
+		/// <summary>
+		/// Pure imaginary one
+		/// </summary>
+		public static Variable ImaginaryOne { get; } = new Variable(Complex.ImaginaryOne);
+
+		/// <summary>
+		/// Pure imaginary negative one
+		/// </summary>
+		public static Variable NegativeImaginaryOne { get; } = new Variable(-Complex.ImaginaryOne);
+
+		#endregion
+	}
 }
