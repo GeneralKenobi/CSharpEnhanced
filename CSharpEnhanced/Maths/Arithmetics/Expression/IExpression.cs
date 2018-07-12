ec059899f64e07ec4edf1d4defb361ede0da7c74
Szymon Żogała <szymek.655@gmail.com>
2018-07-12 21:22:40 +0200

    Added

---
+++ CSharpEnhanced/Maths/Arithmetics/Expression/IExpression.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+namespace CSharpEnhanced.Maths
+{
+	/// <summary>
+	/// Interface for algebraic expressions
+	/// </summary>
+	public interface IExpression
+    {
+		#region Methods
+
+		/// <summary>
+		/// Returns the value of the <see cref="IExpression"/>
+		/// </summary>
+		/// <returns></returns>
+		Complex Evaluate();
+
+		/// <summary>
+		/// Returns a negation of the <see cref="IExpression"/>
+		/// </summary>
+		/// <returns></returns>
+		IExpression Negation();
+
+		/// <summary>
+		/// Returns the reciprocal of the <see cref="IExpression"/>
+		/// </summary>
+		/// <returns></returns>
+		IExpression Reciprocal();
+
+		#endregion
+	}
+}
