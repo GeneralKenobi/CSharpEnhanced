c5a4200ed28b6cbba6906e6ce22baf5ef4a883d2
Szymon Żogała <szymek.655@gmail.com>
2018-07-12 23:24:50 +0200

    Hopefully the final implementation

--- CSharpEnhanced/Maths/Arithmetics/Expression/IExpression.cs
+++ CSharpEnhanced/Maths/Arithmetics/Expression/IExpression.cs
@@ -27,6 +27,34 @@ namespace CSharpEnhanced.Maths
 		/// <returns></returns>
 		IExpression Reciprocal();
 
+		/// <summary>
+		/// Adds two expressions
+		/// </summary>
+		/// <param name="expression"></param>
+		/// <returns></returns>
+		IExpression Add(IExpression expression);
+
+		/// <summary>
+		/// Subtracts <paramref name="expression"/> from this <see cref="IExpression"/>
+		/// </summary>
+		/// <param name="expression"></param>
+		/// <returns></returns>
+		IExpression Subtract(IExpression expression);
+
+		/// <summary>
+		/// Mutliplies two <see cref="IExpression"/>s
+		/// </summary>
+		/// <param name="expression"></param>
+		/// <returns></returns>
+		IExpression Multiply(IExpression expression);
+
+		/// <summary>
+		/// Divides this <see cref="IExpression"/> by <paramref name="expression"/>
+		/// </summary>
+		/// <param name="expression"></param>
+		/// <returns></returns>
+		IExpression Divide(IExpression expression);
+
 		#endregion
 	}
 }
