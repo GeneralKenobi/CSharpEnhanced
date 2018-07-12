bf56e31be717380c153754155fc4aac34d2de742
Szymon Żogała <szymek.655@gmail.com>
2018-07-12 10:01:09 +0200

    Added constructors

--- CSharpEnhanced/CoreClasses/RefWrapper/RefWrapper.cs
+++ CSharpEnhanced/CoreClasses/RefWrapper/RefWrapper.cs
@@ -5,10 +5,31 @@
 	/// </summary>
 	/// <typeparam name="T"></typeparam>
 	public class RefWrapper<T>
-    {
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public RefWrapper() { }
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		public RefWrapper(T value)
+		{
+			Value = value;
+		}
+
+		#endregion
+
+		#region Public properties
+
 		/// <summary>
 		/// Stored value
 		/// </summary>
 		public T Value { get; set; }
-    }
+
+		#endregion
+	}
 }
