f16a0622d3012894dbd3a4676197319887ef04bd
Szymon Żogała <szymek.655@gmail.com>
2018-07-13 21:56:24 +0200

    Seems to be working, needs optimization, refactoring

--- CSharpEnhanced/Maths/Arithmetics/SystemOfLinearEquations/SystemOfLinearEquations.cs
+++ CSharpEnhanced/Maths/Arithmetics/SystemOfLinearEquations/SystemOfLinearEquations.cs
@@ -1,132 +1,170 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Numerics;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Text;
 
-//namespace CSharpEnhanced.Maths
-//{
-//    public class SystemOfLinearEquations
-//    {
-//		/// <summary>
-//		/// Matrix of coefficients
-//		/// </summary>
-//		public Expression[,] Coefficients { get; set; }
+namespace CSharpEnhanced.Maths
+{
+	public class SystemOfLinearEquations
+	{
+		/// <summary>
+		/// Matrix of coefficients
+		/// </summary>
+		public IExpression[,] Coefficients { get; set; }
 
-//		/// <summary>
-//		/// Matrix of free terms
-//		/// </summary>
-//		public Expression[] FreeTerms { get; set; }
+		/// <summary>
+		/// Matrix of free terms
+		/// </summary>
+		public IExpression[] FreeTerms { get; set; }
 
 
-//		public int Rows => FreeTerms.Length;
+		public int Rows => FreeTerms.Length;
 
-//		public int Columns => Coefficients.GetLength(1);
+		public int Columns => Coefficients.GetLength(1);
 
-//		/// <summary>
-//		/// Solves the system with use of partial gauss elimination
-//		/// </summary>
-//		public void Solve()
-//		{
-//			for(int i=0; i<Rows; ++i)
-//			{
-//				DivideRowByDiagonal(i);
-//			}
-//		}
+		/// <summary>
+		/// Solves the system with use of partial gauss elimination
+		/// </summary>
+		public void Solve()
+		{
+			for (int i = 0; i < Rows; ++i)
+			{
+				DivideRowByDiagonal(i);
+				
+				for(int j = i + 1; j < Rows; ++j)
+				{
+					SubtractRowFromRow(j, i, Coefficients[j,i]);					
+				}
+			}
+			
+			for (int i = Rows - 1; i >= 0; --i)
+			{
+				for (int j = i - 1; j >= 0; --j)
+				{
+					SubtractRowFromRow(j, i, Coefficients[j,i]);
+				}
+			}
+		}
 
-//		/// <summary>
-//		/// Adds respective entries from row <paramref name="rowToAdd"/> to entries in row <paramref name="rowToAddTo"/>
-//		/// </summary>
-//		/// <param name="rowToAddTo"></param>
-//		/// <param name="rowToAdd"></param>
-//		private void AddRowToRow(int rowToAddTo, int rowToAdd) => AddRowToRow(rowToAddTo, rowToAdd, Variable.One);
+		/// <summary>
+		/// Adds respective entries from row <paramref name="rowToAdd"/> to entries in row <paramref name="rowToAddTo"/>
+		/// </summary>
+		/// <param name="rowToAddTo"></param>
+		/// <param name="rowToAdd"></param>
+		private void AddRowToRow(int rowToAddTo, int rowToAdd)
+		{
+			for (int i = 0; i < Columns; ++i)
+			{
+				Coefficients[rowToAddTo, i] = Coefficients[rowToAddTo, i].Add(Coefficients[rowToAdd, i]);
+			}
 
-//		/// <summary>
-//		/// Adds respective entries from row <paramref name="rowToAdd"/> multiplied by <paramref name="multiplier"/>
-//		/// to entries in row <paramref name="rowToAddTo"/>
-//		/// </summary>
-//		/// <param name="rowToAddTo"></param>
-//		/// <param name="rowToAdd"></param>
-//		/// <param name="multiplier"></param>
-//		private void AddRowToRow(int rowToAddTo, int rowToAdd, Product multiplier)
-//		{
-//			for(int i = 0; i< Columns; ++i)
-//			{
-//				Coefficients[rowToAddTo, i] += Coefficients[rowToAdd, i] * multiplier;
-//			}
+			FreeTerms[rowToAddTo] = FreeTerms[rowToAddTo].Add(FreeTerms[rowToAdd]);
+		}
 
-//			FreeTerms[rowToAddTo] += FreeTerms[rowToAdd] * multiplier;
-//		}
+		/// <summary>
+		/// Adds respective entries from row <paramref name="rowToAdd"/> multiplied by <paramref name="multiplier"/>
+		/// to entries in row <paramref name="rowToAddTo"/>
+		/// </summary>
+		/// <param name="rowToAddTo"></param>
+		/// <param name="rowToAdd"></param>
+		/// <param name="multiplier"></param>
+		private void AddRowToRow(int rowToAddTo, int rowToAdd, IExpression multiplier)
+		{
+			for (int i = 0; i < Columns; ++i)
+			{
+				Coefficients[rowToAddTo, i] = Coefficients[rowToAddTo, i].Add(Coefficients[rowToAdd, i].Multiply(multiplier));
+			}
 
-//		/// <summary>
-//		/// Subtracts respective entries from row <paramref name="rowToSubtract"/> from entries in row
-//		/// <paramref name="rowToSubtractFrom"/>
-//		/// </summary>
-//		/// <param name="rowToSubtractFrom"></param>
-//		/// <param name="rowToSubtract"></param>
-//		private void SubtractRowFromRow(int rowToSubtractFrom, int rowToSubtract) =>
-//			AddRowToRow(rowToSubtractFrom, rowToSubtract, Variable.NegativeOne);
+			FreeTerms[rowToAddTo] = FreeTerms[rowToAddTo].Add(FreeTerms[rowToAdd].Multiply(multiplier));
+		}
 
-//		/// <summary>
-//		/// Subtracts respective entries from row <paramref name="rowToSubtract"/> multiplied by <paramref name="multiplier"/>
-//		/// from entries in row <paramref name="rowToSubtractFrom"/>
-//		/// </summary>
-//		/// <param name="rowToSubtractFrom"></param>
-//		/// <param name="rowToSubtract"></param>
-//		/// <param name="multiplier"></param>
-//		private void SubtractRowFromRow(int rowToSubtractFrom, int rowToSubtract, Product multiplier) =>
-//			AddRowToRow(rowToSubtractFrom, rowToSubtract, -multiplier);
+		/// <summary>
+		/// Subtracts respective entries from row <paramref name="rowToSubtract"/> from entries in row
+		/// <paramref name="rowToSubtractFrom"/>
+		/// </summary>
+		/// <param name="rowToSubtractFrom"></param>
+		/// <param name="rowToSubtract"></param>
+		private void SubtractRowFromRow(int rowToSubtractFrom, int rowToSubtract)
+		{
+			for (int i = 0; i < Columns; ++i)
+			{
+				Coefficients[rowToSubtractFrom, i] = Coefficients[rowToSubtractFrom, i].Subtract(Coefficients[rowToSubtract, i]);
+			}
 
-//		/// <summary>
-//		/// Multiplies a row by the given number
-//		/// </summary>
-//		/// <param name="row"></param>
-//		/// <param name="number"></param>
-//		private void MultiplyRowBy(int row, Product number)
-//		{
-//			for (int i = 0; i < Columns; ++i)
-//			{
-//				Coefficients[row,i] *= number;
-//			}
+			FreeTerms[rowToSubtractFrom] = FreeTerms[rowToSubtractFrom].Subtract(FreeTerms[rowToSubtract]);
+		}
 
-//			FreeTerms[row] *= number;
-//		}
 
-//		/// <summary>
-//		/// Divides a row by the given number
-//		/// </summary>
-//		/// <param name="row"></param>
-//		/// <param name="number"></param>
-//		private void DivideRowBy(int row, Product number) => MultiplyRowBy(row, number.Reciprocal());
+		/// <summary>
+		/// Subtracts respective entries from row <paramref name="rowToSubtract"/> multiplied by <paramref name="multiplier"/>
+		/// from entries in row <paramref name="rowToSubtractFrom"/>
+		/// </summary>
+		/// <param name="rowToSubtractFrom"></param>
+		/// <param name="rowToSubtract"></param>
+		/// <param name="multiplier"></param>
+		private void SubtractRowFromRow(int rowToSubtractFrom, int rowToSubtract, IExpression multiplier)
+		{
+			for (int i = 0; i < Columns; ++i)
+			{
+				Coefficients[rowToSubtractFrom, i] =
+					Coefficients[rowToSubtractFrom, i].Subtract(Coefficients[rowToSubtract, i].Multiply(multiplier));
+			}
 
-//		private void DivideRowByDiagonal(int row)
-//		{
-//			var divider = Coefficients[row, row];
+			FreeTerms[rowToSubtractFrom] = FreeTerms[rowToSubtractFrom].Subtract(FreeTerms[rowToSubtract].Multiply(multiplier));
+		}
 
-//			for (int i = 0; i < Columns; ++i)
-//			{
-//				Coefficients[row, i] /= divider;
-//			}
+		/// <summary>
+		/// Multiplies a row by the given number
+		/// </summary>
+		/// <param name="row"></param>
+		/// <param name="number"></param>
+		private void MultiplyRowBy(int row, IExpression number)
+		{
+			for (int i = 0; i < Columns; ++i)
+			{
+				Coefficients[row, i] = Coefficients[row,i].Multiply(number);
+			}
 
-//			FreeTerms[row] /= divider;
-//		}
+			FreeTerms[row] = FreeTerms[row].Multiply(number);
+		}
 
-//		/// <summary>
-//		/// Swaps two rows
-//		/// </summary>
-//		/// <param name="row1"></param>
-//		/// <param name="row2"></param>
-//		private void SwapRows(int row1, int row2)
-//		{
-//			var temp = FreeTerms[row1];
-//			FreeTerms[row1] = FreeTerms[row2];
-//			FreeTerms[row2] = temp;
+		/// <summary>
+		/// Divides a row by the given number
+		/// </summary>
+		/// <param name="row"></param>
+		/// <param name="number"></param>
+		private void DivideRowBy(int row, IExpression number) => MultiplyRowBy(row, number.Reciprocal());
 
-//			for(int i=0; i<Columns; ++i)
-//			{
-//				temp = Coefficients[row1,i];
-//				Coefficients[row1, i] = Coefficients[row2, i];
-//				Coefficients[row2, i] = temp;
-//			}
-//		}
-//	}
-//}
+		private void DivideRowByDiagonal(int row)
+		{
+			var divider = Coefficients[row, row];
+
+			for (int i = 0; i < Columns; ++i)
+			{
+				Coefficients[row, i] = Coefficients[row, i].Divide(divider);
+			}
+
+			FreeTerms[row] = FreeTerms[row].Divide(divider);
+		}
+
+		/// <summary>
+		/// Swaps two rows
+		/// </summary>
+		/// <param name="row1"></param>
+		/// <param name="row2"></param>
+		private void SwapRows(int row1, int row2)
+		{
+			var temp = FreeTerms[row1];
+			FreeTerms[row1] = FreeTerms[row2];
+			FreeTerms[row2] = temp;
+
+			for (int i = 0; i < Columns; ++i)
+			{
+				temp = Coefficients[row1, i];
+				Coefficients[row1, i] = Coefficients[row2, i];
+				Coefficients[row2, i] = temp;
+			}
+		}
+	}
+}
--- TestEnvironment/Program.cs
+++ TestEnvironment/Program.cs
@@ -14,20 +14,49 @@ namespace TestEnvironment
     {
 		static void Main(string[] args)
 		{
-			var sx = new Variable.VariableSource(2, "x");
-			var sy = new Variable.VariableSource(5, "y");
-			var sz = new Variable.VariableSource(-3, "z");
+			var sx = new Variable.VariableSource(2, -3, "x");
+			var sy = new Variable.VariableSource(5, 2, "y");
+			var sz = new Variable.VariableSource(10, "z");
+			var sw = new Variable.VariableSource(4, "w");
 
 			var x = sx.Variable;
 			var y = sy.Variable;
 			var z = sz.Variable;
+			var w = sw.Variable;
 
-			IExpression exp = x.Add(y).Multiply(z).Subtract(x.Multiply(y)).Divide(z.Multiply(y)).Divide(x.Add(y));
+			//IExpression exp = x.Add(y).Multiply(z).Subtract(x.Multiply(y)).Divide(z.Multiply(y)).Divide(x.Add(y));
 
-			Console.Write(exp.ToString());
+			//Console.Write(exp.ToString());
 
 
-			Console.Write($"\t = {exp.Evaluate()}");
+			//Console.Write($"\t = {exp.Evaluate()}");
+
+
+			var system = new SystemOfLinearEquations();
+
+			system.Coefficients = new IExpression[,] { { x, y, z}, { y, z, w}, { z, w, x } };
+			system.FreeTerms = new IExpression[] { w, x, y};
+			//system.Coefficients = new IExpression[,] { { x, w }, { w, y } };
+			//system.FreeTerms = new IExpression[] { z, z };
+			Stopwatch s = new Stopwatch();
+			s.Start();
+			system.Solve();
+			s.Stop();
+			//Console.WriteLine(s.ElapsedMilliseconds);
+
+			Console.WriteLine(system.FreeTerms[0].Evaluate());
+			Console.WriteLine(system.FreeTerms[1].Evaluate());
+			Console.WriteLine(system.FreeTerms[2].Evaluate());
+			
+			for(int i=0; i<system.Rows; ++i)
+			{
+				for(int j=0; j<system.Columns; ++j)
+				{
+					Console.Write(system.Coefficients[i, j].Evaluate().ToString() + "\t");
+				}
+
+				Console.WriteLine();
+			}
 
 			Console.ReadLine();
 		}
