070041c84e13351c7142e8eaa14de212981d916d
Szymon Żogała <szymek.655@gmail.com>
2018-07-13 17:28:21 +0200

    Beginning to work on SystemOfLinearEquations with IExppresion

--- TestEnvironment/Program.cs
+++ TestEnvironment/Program.cs
@@ -1,8 +1,10 @@
 ﻿using CSharpEnhanced.Helpers;
+using CSharpEnhanced.Maths;
 using CSharpEnhanced.Synchronization;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,7 +13,23 @@ namespace TestEnvironment
 	class Program
     {
 		static void Main(string[] args)
-		{		
+		{
+			var sx = new Variable.VariableSource(2, "x");
+			var sy = new Variable.VariableSource(5, "y");
+			var sz = new Variable.VariableSource(-3, "z");
+
+			var x = sx.Variable;
+			var y = sy.Variable;
+			var z = sz.Variable;
+
+			IExpression exp = x.Add(y).Multiply(z).Subtract(x.Multiply(y)).Divide(z.Multiply(y)).Divide(x.Add(y));
+
+			Console.Write(exp.ToString());
+
+
+			Console.Write($"\t = {exp.Evaluate()}");
+
+			Console.ReadLine();
 		}
 	}
 }
