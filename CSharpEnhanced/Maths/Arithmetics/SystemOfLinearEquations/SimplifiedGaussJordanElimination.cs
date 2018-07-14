f2afbeb54707ed8a20d859742819542dc7037250
Szymon Żogała <szymek.655@gmail.com>
2018-07-14 21:04:29 +0200

    Restructurized the class

---
+++ CSharpEnhanced/Maths/Arithmetics/SystemOfLinearEquations/SimplifiedGaussJordanElimination.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpEnhanced.Maths
+{
+    public partial class LinearEquations
+    {
+		/// <summary>
+		/// Solves the system with use of partial gauss elimination
+		/// </summary>
+		public static IExpression[] SimplifiedGaussJordanElimination(IExpression[,] coefficients, IExpression[] freeTerms)
+		{
+			// Check for null
+			if(coefficients == null || freeTerms == null)
+			{
+				throw new ArgumentNullException();
+			}
+
+			// Check if the dimensions match
+			if (coefficients.GetLength(0) == coefficients.GetLength(1) &&
+				coefficients.GetLength(1) == freeTerms.Length)
+			{
+				// Create a new solver instance
+				var solver = new SimplifiedGaussJordanEliminationSolver(coefficients, freeTerms);
+				
+				// Perform necessary operations
+				solver.ForwardElemination();
+				solver.BackwardsElimination();
+
+				// Return the result
+				return solver.FreeTerms;
+			}
+			else
+			{
+				throw new ArgumentException(nameof(coefficients) + " is not a square matrix or its size does not" +
+					" equal the length of " + nameof(freeTerms));
+			}
+		}
+
+		private class SimplifiedGaussJordanEliminationSolver : LinearEquations
+		{
+
+			/// <summary>
+			/// Default Constructor
+			/// </summary>
+			public SimplifiedGaussJordanEliminationSolver(IExpression[,] coefficients, IExpression[] freeTerms)
+				: base(coefficients, freeTerms)	{ }
+
+			public void ForwardElemination()
+			{
+				for (int i = 0; i < Size; ++i)
+				{
+					DivideRowByDiagonal(i);
+
+					for (int j = i + 1; j < Size; ++j)
+					{
+						SubtractRows(j, i, Coefficients[j, i], i + 1, Size);
+					}
+				}
+			}
+
+			public void BackwardsElimination()
+			{
+				for (int i = Size - 1; i >= 0; --i)
+				{
+					for (int j = i - 1; j >= 0; --j)
+					{
+						SubtractFreeTerms(j, i, Coefficients[j, i]);
+					}
+				}
+			}
+
+			private void SubtractRows(int rowToSubtractFrom, int rowToSubtract, IExpression multiplier,
+				int startFromColumn, int endAtColumn)
+			{
+				for (int i = startFromColumn; i < endAtColumn; ++i)
+				{
+					Coefficients[rowToSubtractFrom, i] =
+						Coefficients[rowToSubtractFrom, i].Subtract(Coefficients[rowToSubtract, i].Multiply(multiplier));
+				}
+
+				FreeTerms[rowToSubtractFrom] = FreeTerms[rowToSubtractFrom].Subtract(FreeTerms[rowToSubtract].Multiply(multiplier));
+			}
+
+			private void SubtractFreeTerms(int rowToSubtractFrom, int rowToSubtract, IExpression multiplier) =>
+				FreeTerms[rowToSubtractFrom] = FreeTerms[rowToSubtractFrom].Subtract(FreeTerms[rowToSubtract].Multiply(multiplier));
+
+			private void DivideRowByDiagonal(int row)
+			{
+				var divider = Coefficients[row, row];
+
+				for (int i = row; i < Size; ++i)
+				{
+					Coefficients[row, i] = Coefficients[row, i].Divide(divider);
+				}
+
+				FreeTerms[row] = FreeTerms[row].Divide(divider);
+			}
+		}
+	}
+}
--- CSharpEnhanced/Maths/Arithmetics/SystemOfLinearEquations/SystemOfLinearEquations.cs
+++ CSharpEnhanced/Maths/Arithmetics/SystemOfLinearEquations/SystemOfLinearEquations.cs
@@ -1,86 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Numerics;
-using System.Text;
+﻿// This file contains the LinearEquations class which is a base that exposes some properties that are used in every
+// method of solving linear equations. Each method should be put in a seperate file that adds to the partial definition
+// of this class. The implementations should expose static methods that use this base
+
 
 namespace CSharpEnhanced.Maths
 {
-	public class SystemOfLinearEquations
+	/// <summary>
+	/// Solves systems of linear equations using the chosen method
+	/// </summary>
+	public partial class LinearEquations
 	{
+		/// <summary>
+		/// Default Constructor, methods that use it should make sure that the requirements they present for these
+		/// matrices are fulfilled
+		/// </summary>
+		protected LinearEquations(IExpression[,] coefficients, IExpression[] freeTerms)
+		{
+			Coefficients = coefficients;
+			FreeTerms = freeTerms;
+		}
+
 		/// <summary>
 		/// Matrix of coefficients
 		/// </summary>
-		public IExpression[,] Coefficients { get; set; }
+		protected IExpression[,] Coefficients { get; set; }
 
 		/// <summary>
 		/// Matrix of free terms
 		/// </summary>
-		public IExpression[] FreeTerms { get; set; }
+		protected IExpression[] FreeTerms { get; set; }
 
-
-		public int Rows => FreeTerms.Length;
-
-		public int Columns => Coefficients.GetLength(1);
-
-		/// <summary>
-		/// Solves the system with use of partial gauss elimination
-		/// </summary>
-		public void Solve()
-		{
-			ForwardElemination();
-			BackwardsElimination();
-		}
-
-		private void ForwardElemination()
-		{
-			for (int i = 0; i < Rows; ++i)
-			{
-				DivideRowByDiagonal(i);
-
-				for (int j = i + 1; j < Rows; ++j)
-				{
-					SubtractRows(j, i, Coefficients[j, i], i + 1, Columns);
-				}
-			}
-		}
-
-		private void BackwardsElimination()
-		{
-			for (int i = Rows - 1; i >= 0; --i)
-			{
-				for (int j = i - 1; j >= 0; --j)
-				{
-					SubtractFreeTerms(j, i, Coefficients[j, i]);
-				}
-			}
-		}
-
-		private void SubtractRows(int rowToSubtractFrom, int rowToSubtract, IExpression multiplier,
-			int startFromColumn, int endAtColumn)
-		{
-			for (int i = startFromColumn; i < endAtColumn; ++i)
-			{
-				Coefficients[rowToSubtractFrom, i] =
-					Coefficients[rowToSubtractFrom, i].Subtract(Coefficients[rowToSubtract, i].Multiply(multiplier));
-			}
-
-			FreeTerms[rowToSubtractFrom] = FreeTerms[rowToSubtractFrom].Subtract(FreeTerms[rowToSubtract].Multiply(multiplier));
-		}
-
-		private void SubtractFreeTerms(int rowToSubtractFrom, int rowToSubtract, IExpression multiplier) =>
-			FreeTerms[rowToSubtractFrom] = FreeTerms[rowToSubtractFrom].Subtract(FreeTerms[rowToSubtract].Multiply(multiplier));
-
-		private void DivideRowByDiagonal(int row)
-		{
-			var divider = Coefficients[row, row];
-
-			for (int i = row; i < Columns; ++i)
-			{
-				Coefficients[row, i] = Coefficients[row, i].Divide(divider);
-			}
-
-			FreeTerms[row] = FreeTerms[row].Divide(divider);
-		}
+		// The size of the system - number of equations
+		protected int Size => FreeTerms.Length;
 	}
 }
