c653adaf8616cd675fb94f4f453bd4805cf2d49f
Szymon Żogała <szymek.655@gmail.com>
2018-07-14 18:45:32 +0200

    Removed redundant methods

--- CSharpEnhanced/Maths/Arithmetics/SystemOfLinearEquations/SystemOfLinearEquations.cs
+++ CSharpEnhanced/Maths/Arithmetics/SystemOfLinearEquations/SystemOfLinearEquations.cs
@@ -48,55 +48,6 @@ namespace CSharpEnhanced.Maths
 		}
 
 		/// <summary>
-		/// Adds respective entries from row <paramref name="rowToAdd"/> to entries in row <paramref name="rowToAddTo"/>
-		/// </summary>
-		/// <param name="rowToAddTo"></param>
-		/// <param name="rowToAdd"></param>
-		private void AddRowToRow(int rowToAddTo, int rowToAdd)
-		{
-			for (int i = 0; i < Columns; ++i)
-			{
-				Coefficients[rowToAddTo, i] = Coefficients[rowToAddTo, i].Add(Coefficients[rowToAdd, i]);
-			}
-
-			FreeTerms[rowToAddTo] = FreeTerms[rowToAddTo].Add(FreeTerms[rowToAdd]);
-		}
-
-		/// <summary>
-		/// Adds respective entries from row <paramref name="rowToAdd"/> multiplied by <paramref name="multiplier"/>
-		/// to entries in row <paramref name="rowToAddTo"/>
-		/// </summary>
-		/// <param name="rowToAddTo"></param>
-		/// <param name="rowToAdd"></param>
-		/// <param name="multiplier"></param>
-		private void AddRowToRow(int rowToAddTo, int rowToAdd, IExpression multiplier)
-		{
-			for (int i = 0; i < Columns; ++i)
-			{
-				Coefficients[rowToAddTo, i] = Coefficients[rowToAddTo, i].Add(Coefficients[rowToAdd, i].Multiply(multiplier));
-			}
-
-			FreeTerms[rowToAddTo] = FreeTerms[rowToAddTo].Add(FreeTerms[rowToAdd].Multiply(multiplier));
-		}
-
-		/// <summary>
-		/// Subtracts respective entries from row <paramref name="rowToSubtract"/> from entries in row
-		/// <paramref name="rowToSubtractFrom"/>
-		/// </summary>
-		/// <param name="rowToSubtractFrom"></param>
-		/// <param name="rowToSubtract"></param>
-		private void SubtractRowFromRow(int rowToSubtractFrom, int rowToSubtract)
-		{
-			for (int i = 0; i < Columns; ++i)
-			{
-				Coefficients[rowToSubtractFrom, i] = Coefficients[rowToSubtractFrom, i].Subtract(Coefficients[rowToSubtract, i]);
-			}
-
-			FreeTerms[rowToSubtractFrom] = FreeTerms[rowToSubtractFrom].Subtract(FreeTerms[rowToSubtract]);
-		}
-
-
-		/// <summary>
 		/// Subtracts respective entries from row <paramref name="rowToSubtract"/> multiplied by <paramref name="multiplier"/>
 		/// from entries in row <paramref name="rowToSubtractFrom"/>
 		/// </summary>
@@ -129,13 +80,6 @@ namespace CSharpEnhanced.Maths
 			FreeTerms[row] = FreeTerms[row].Multiply(number);
 		}
 
-		/// <summary>
-		/// Divides a row by the given number
-		/// </summary>
-		/// <param name="row"></param>
-		/// <param name="number"></param>
-		private void DivideRowBy(int row, IExpression number) => MultiplyRowBy(row, number.Reciprocal());
-
 		private void DivideRowByDiagonal(int row)
 		{
 			var divider = Coefficients[row, row];
@@ -146,25 +90,6 @@ namespace CSharpEnhanced.Maths
 			}
 
 			FreeTerms[row] = FreeTerms[row].Divide(divider);
-		}
-
-		/// <summary>
-		/// Swaps two rows
-		/// </summary>
-		/// <param name="row1"></param>
-		/// <param name="row2"></param>
-		private void SwapRows(int row1, int row2)
-		{
-			var temp = FreeTerms[row1];
-			FreeTerms[row1] = FreeTerms[row2];
-			FreeTerms[row2] = temp;
-
-			for (int i = 0; i < Columns; ++i)
-			{
-				temp = Coefficients[row1, i];
-				Coefficients[row1, i] = Coefficients[row2, i];
-				Coefficients[row2, i] = temp;
-			}
 		}
 	}
 }
