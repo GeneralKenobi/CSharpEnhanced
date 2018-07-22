049e8fa0390f8ffe5cd0d3cdd2b3ad3c8e089280
Szymon Żogała <szymek.655@gmail.com>
2018-07-22 20:43:39 +0200

    Added a RefWrapper with INotifyPropertyChanged

---
+++ CSharpEnhanced/CoreClasses/RefWrapper/RefWrapperPropertyChanged.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+
+namespace CSharpEnhanced.CoreClasses
+{
+	/// <summary>
+	/// Class that may be used to wrap non-reference classes to be able to store them by reference that also implements
+	/// <see cref="INotifyPropertyChanged"/>
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class RefWrapperPropertyChanged<T> : INotifyPropertyChanged
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public RefWrapperPropertyChanged() { }
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		public RefWrapperPropertyChanged(T value)
+		{
+			Value = value;
+		}
+
+		#endregion
+		
+		#region Events
+
+		/// <summary>
+		/// Event fired whenever a property changes its value
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		#region Private properties
+
+		/// <summary>
+		/// Backing store for <see cref="Value"/>
+		/// </summary>
+		private T _Value { get; set; }
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Stored value
+		/// </summary>
+		public T Value
+		{
+			get => _Value;
+			set
+			{
+				if(!_Value.Equals(value))
+				{
+					_Value = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+				}
+			}
+		}
+
+		#endregion
+	}
+}
