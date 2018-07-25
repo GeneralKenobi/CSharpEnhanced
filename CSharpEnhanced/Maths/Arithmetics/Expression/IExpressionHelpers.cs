cce72e9ccd9662b2c42b3d7913bd58d24d0191e1
Szymon Żogała <szymek.655@gmail.com>
2018-07-25 22:15:25 +0200

    Fixed indexing bug

--- CSharpEnhanced/Maths/Arithmetics/Expression/IExpressionHelpers.cs
+++ CSharpEnhanced/Maths/Arithmetics/Expression/IExpressionHelpers.cs
@@ -35,9 +35,9 @@ namespace CSharpEnhanced.Maths
 		{
 			var result = new Complex[array.GetLength(0), array.GetLength(1)];
 
-			for (int i = 0; i < array.Length; ++i)
+			for (int i = 0; i < array.GetLength(0); ++i)
 			{
-				for (int j = 0; j < array.Length; ++j)
+				for (int j = 0; j < array.GetLength(1); ++j)
 				{
 					result[i, j] = array[i, j].Evaluate();
 				}
