e6adb1f5a8fdfeba7b6a0025379d7192f61f1218
Szymon Żogała <szymek.655@gmail.com>
2018-07-25 22:07:12 +0200

    Added

---
+++ CSharpEnhanced/Maths/Arithmetics/Expression/IExpressionHelpers.cs
@@ -0,0 +1,51 @@
+﻿using System.Numerics;
+
+namespace CSharpEnhanced.Maths
+{
+	/// <summary>
+	/// Helper extension methods for <see cref="IExpression"/>
+	/// </summary>
+	public static class IExpressionHelpers
+    {
+		#region Public static methods
+
+		/// <summary>
+		/// Evaluates the array and returns it
+		/// </summary>
+		/// <param name="array"></param>
+		/// <returns></returns>
+		public static Complex[] Evaluate(this IExpression[] array)
+		{
+			var result = new Complex[array.Length];
+
+			for(int i=0; i<array.Length; ++i)
+			{
+				result[i] = array[i].Evaluate();
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Evaluates the array and returns it
+		/// </summary>
+		/// <param name="array"></param>
+		/// <returns></returns>
+		public static Complex[,] Evaluate(this IExpression[,] array)
+		{
+			var result = new Complex[array.GetLength(0), array.GetLength(1)];
+
+			for (int i = 0; i < array.Length; ++i)
+			{
+				for (int j = 0; j < array.Length; ++j)
+				{
+					result[i, j] = array[i, j].Evaluate();
+				}
+			}
+
+			return result;
+		}
+
+		#endregion
+	}
+}
