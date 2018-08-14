3484a74f0e8a795d7fdef15c25a01c364ef5f0e5
Szymon Żogała <szymek.655@gmail.com>
2018-08-14 15:09:37 +0200

    Added

---
+++ CSharpEnhanced/CoreClasses/KeyInput/KeyArgument.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharpEnhanced.CoreClasses
+{
+	/// <summary>
+	/// Wraps a key event into a class containing a string representation of the key and key modifiers present during key press
+	/// </summary>
+	public class KeyArgument
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public KeyArgument(string key, KeyModifiers modifiers)
+		{
+			Key = key;
+			Modifiers = modifiers;
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Key that was pressed
+		/// </summary>
+		public string Key { get; }
+
+		/// <summary>
+		/// Modifiers active 
+		/// </summary>
+		public KeyModifiers Modifiers { get; }
+
+		#endregion
+	}
+}
