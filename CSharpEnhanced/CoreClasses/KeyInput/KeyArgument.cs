18237b8123fd4af02eecd092ae6f4d5a29f99a63
Szymon Żogała <szymek.655@gmail.com>
2018-08-16 21:19:22 +0200

    Added an Equals override, a default parameter to constructor and an empty KeyArgument as a static property

--- CSharpEnhanced/CoreClasses/KeyInput/KeyArgument.cs
+++ CSharpEnhanced/CoreClasses/KeyInput/KeyArgument.cs
@@ -10,7 +10,7 @@
 		/// <summary>
 		/// Default constructor
 		/// </summary>
-		public KeyArgument(string key, KeyModifiers modifiers)
+		public KeyArgument(string key, KeyModifiers modifiers = KeyModifiers.None)
 		{
 			Key = key;
 			Modifiers = modifiers;
@@ -29,6 +29,41 @@
 		/// Modifiers active 
 		/// </summary>
 		public KeyModifiers Modifiers { get; }
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Compares two <see cref="KeyArgument"/>s, if both public properties are equal then the objects are equal.
+		/// Keys are not case sensitive.
+		/// </summary>
+		/// <param name="obj"></param>
+		/// <returns></returns>
+		public override bool Equals(object obj)
+		{
+			if (obj is KeyArgument shortcut)
+			{
+				return Key.ToUpper() == shortcut.Key.ToUpper() && Modifiers == shortcut.Modifiers;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Returns a unique hash code for the instance
+		/// </summary>
+		/// <returns></returns>
+		public override int GetHashCode() => Key.ToUpper().GetHashCode() * 17 + Modifiers.GetHashCode();
+
+		#endregion
+
+		#region Public static properties
+
+		/// <summary>
+		/// Empty key argument
+		/// </summary>
+		public static KeyArgument Empty { get; } = new KeyArgument(string.Empty);
 
 		#endregion
 	}
