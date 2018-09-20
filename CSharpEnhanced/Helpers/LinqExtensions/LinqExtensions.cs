3067bd06116f0e292082ed346493e26f550e2e25
Szymon Żogała <szymek.655@gmail.com>
2018-09-20 22:44:58 +0200

    Added ForEach method

--- CSharpEnhanced/Helpers/LinqExtensions/LinqExtensions.cs
+++ CSharpEnhanced/Helpers/LinqExtensions/LinqExtensions.cs
@@ -22,6 +22,22 @@ namespace CSharpEnhanced.Helpers
 			// Check if enumeration isnt' null, cast items to IEnumerable<T> to use the linq method (otherwise recursion would occur)
 			(enumeration ?? throw new ArgumentNullException(nameof(enumeration))).Concat(items as IEnumerable<T>);
 
+		/// <summary>
+		/// Performs a foreach loop on enumeration with the given action (enabling inline calls). Returns <paramref name="enumeration"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="enumeration"></param>
+		/// <param name="action"></param>
+		public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+		{
+			foreach(var item in enumeration)
+			{
+				action(item);
+			}
+
+			return enumeration;
+		}
+
 		#endregion
 	}
 }
