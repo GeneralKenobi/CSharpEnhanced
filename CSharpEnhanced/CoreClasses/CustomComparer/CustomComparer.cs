8c456bf1ede92baae7a19c777e6a233f85e397c4
Szymon Żogała <szymek.655@gmail.com>
2018-09-22 16:30:32 +0200

    Added

---
+++ CSharpEnhanced/CoreClasses/CustomComparer/CustomComparer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpEnhanced.CoreClasses
+{
+	/// <summary>
+	/// <see cref="IComparer{T}"/> that uses a Func given as parameter in constructor for comparison.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class CustomComparer<T> : Comparer<T>
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		/// <param name="comparisonFunc"></param>
+		/// <exception cref="ArgumentNullException"></exception>
+		public CustomComparer(Func<T, T, int> comparisonFunc)
+		{
+			_ComparisonFunc = comparisonFunc ?? throw new ArgumentNullException(nameof(comparisonFunc));
+		}
+
+		#endregion
+
+		#region Private properties
+
+		/// <summary>
+		/// Func used for comparison
+		/// </summary>
+		private Func<T, T, int> _ComparisonFunc { get; }
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Compares two objects using the func given in constructor
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <returns></returns>
+		public override int Compare(T x, T y) => _ComparisonFunc.Invoke(x, y);
+
+		#endregion
+	}
+}
