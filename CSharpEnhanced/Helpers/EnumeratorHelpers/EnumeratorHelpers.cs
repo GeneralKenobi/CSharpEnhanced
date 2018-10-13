fd760a188ab5d0c6aedaaa40b823d36ef8ca957a
Szymon Żogała <szymek.655@gmail.com>
2018-10-13 23:54:03 +0200

    Added methods for advancing enumerator n positions

--- CSharpEnhanced/Helpers/EnumeratorHelpers/EnumeratorHelpers.cs
+++ CSharpEnhanced/Helpers/EnumeratorHelpers/EnumeratorHelpers.cs
@@ -10,6 +10,26 @@ namespace CSharpEnhanced.Helpers
 		#region Public methods
 
 		/// <summary>
+		/// Advances the iterator n elements forward, returns true on success.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="enumerator"></param>
+		/// <param name="n">Number of positions to advance forward</param>
+		/// <returns></returns>
+		public static bool MoveNext<T>(this IEnumerator<T> enumerator, int n)
+		{
+			for(int i=0; i<n; ++i)
+			{
+				if(!enumerator.MoveNext())
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		/// <summary>
 		/// Moves to the next element. If the action exceeded the enumeration, returns to the first element. Returns true on success.
 		/// Returns false on failure - when enumeration is empty and it's not possible to reach any element.
 		/// </summary>
@@ -33,6 +53,27 @@ namespace CSharpEnhanced.Helpers
 				// reach the first element and it's not possible to wrap around the end - it needs to be signaled)
 				return enumerator.MoveNext();
 			}
+		}
+
+		/// <summary>
+		/// Moves n positions forward. If the action exceeded the enumeration, returns to the first element. Returns true on success.
+		/// Returns false on failure - when enumeration is empty and it's not possible to reach any element.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="enumerator"></param>
+		/// <param name="n">Number of positions to advance forward</param>
+		/// <returns></returns>
+		public static bool MoveNextWrapping<T>(this IEnumerator<T> enumerator, int n)
+		{
+			for (int i = 0; i < n; ++i)
+			{
+				if (!enumerator.MoveNextWrapping())
+				{
+					return false;
+				}
+			}
+
+			return true;
 		}
 
 		#endregion
