d7392d1bf21f6694525700a7327775d18275703d
Szymon Żogała <szymek.655@gmail.com>
2018-10-13 23:41:37 +0200

    Removed the wrapping behavior options

--- CSharpEnhanced/Helpers/LinqExtensions/DifferentCountBehavior.cs
+++ CSharpEnhanced/Helpers/LinqExtensions/DifferentCountBehavior.cs
@@ -28,18 +28,5 @@
 		/// in the shorter sequence. Example: for an adding func, sequences a,b,c,d and x,y,z will return bx,cy,dz.
 		/// </summary>
 		TakeEndingOfLonger = 3,
-
-		/// <summary>
-		/// The shorter sequence is wrapped (when enumerator reaches the end it is returned to the first element) until the longer
-		/// sequence is fully enumerated. Example: for an adding func, sequences a,b,c,d and x,y,z will return ax,by,cz,dx
-		/// </summary>
-		WrapShorter = 4,
-
-		/// <summary>
-		/// Enumeration continues (whenever an enumerator reaches the end it is returned to beginning) until both enumerators reach
-		/// the end at the same time. Example: for an adding func sequences a,b,c,d and x,y,z will return
-		/// ax,by,cz,dx,ay,bz,cx,dy,az,bx,cy,dz
-		/// </summary>
-		WrapToFullEnumeration = 5,
 	}
 }
