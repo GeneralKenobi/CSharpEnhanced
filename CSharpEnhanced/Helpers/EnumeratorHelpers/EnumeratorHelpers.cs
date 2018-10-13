9836c7c33760153eae99d60756a073466298bb10
Szymon Żogała <szymek.655@gmail.com>
2018-10-13 23:59:25 +0200

    Changed the generic IEnumerator to non-generic IEnumerator

--- CSharpEnhanced/Helpers/EnumeratorHelpers/EnumeratorHelpers.cs
+++ CSharpEnhanced/Helpers/EnumeratorHelpers/EnumeratorHelpers.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 
 namespace CSharpEnhanced.Helpers
 {
 	/// <summary>
-	/// Contains helper methods related to <see cref="IEnumerator{T}"/>
+	/// Contains helper methods related to <see cref="IEnumerator{T}"/> and <see cref="IEnumerator"/>
 	/// </summary>
 	public static class EnumeratorHelpers
 	{
@@ -16,7 +16,7 @@ namespace CSharpEnhanced.Helpers
 		/// <param name="enumerator"></param>
 		/// <param name="n">Number of positions to advance forward</param>
 		/// <returns></returns>
-		public static bool MoveNext<T>(this IEnumerator<T> enumerator, int n)
+		public static bool MoveNext(this IEnumerator enumerator, int n)
 		{
 			for(int i=0; i<n; ++i)
 			{
@@ -36,7 +36,7 @@ namespace CSharpEnhanced.Helpers
 		/// <typeparam name="T"></typeparam>
 		/// <param name="enumerator"></param>
 		/// <returns></returns>
-		public static bool MoveNextWrapping<T>(this IEnumerator<T> enumerator)
+		public static bool MoveNextWrapping(this IEnumerator enumerator)
 		{
 			// Try to move to the next element
 			if(enumerator.MoveNext())
@@ -63,7 +63,7 @@ namespace CSharpEnhanced.Helpers
 		/// <param name="enumerator"></param>
 		/// <param name="n">Number of positions to advance forward</param>
 		/// <returns></returns>
-		public static bool MoveNextWrapping<T>(this IEnumerator<T> enumerator, int n)
+		public static bool MoveNextWrapping(this IEnumerator enumerator, int n)
 		{
 			for (int i = 0; i < n; ++i)
 			{
